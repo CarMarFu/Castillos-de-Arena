2f9dd4f8ce1e3dcf4866a4d1ca65b3c3a1176803
CarMarFu <carmarfu03@gmail.com>
2025-03-29 02:23:31 +0100

    generado estatico

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -2,6 +2,6 @@ var store = [{
         "title": "Calentamos motores para 2025",
         "excerpt":"Buenos y felices días, Somos un grupo de universitarios que después del éxito del año pasado nos hemos vuelto a tirar a la piscina para montar una nueva edición del Concurso de Castillos de Arena en favor de ATECE. Esperamos que en este tiempo hayáis pensado ideas para esta nueva...","categories": ["diary"],
         "tags": [],
-        "url": "/Castillos-de-Arena/diary/2025/03/28/bienvenidos_playeros.html",
+        "url": "/diary/2025/03/28/bienvenidos_playeros.html",
         "teaser": null
       }]
